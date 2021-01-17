63f66efb599cf77c6483aa05a9f2efb61683b1e0
Mohammad Al-Ahdal <mo@mohammad.dev>
2021-01-17 10:52:59 -0700

    add screens to routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Error404Screen, TrackerScreen } from './screens'
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/tracker" exact component={() => <></>} />
-        <Route component={() => <>404 - page not found</>} />
+        <Route path="/tracker" exact component={TrackerScreen} />
+        <Route component={Error404Screen} />
       </Switch>
     </BrowserRouter>
   );
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,2 @@
-export default {};
+export { Error404Screen } from "./Error404Screen";
+export { TrackerScreen } from "./TrackerScreen";

f38ac060d97213cde5006b731b935f34e27b2f25
Mohammad Al-Ahdal <mo@mohammad.dev>
2021-01-17 19:01:17 -0700

    update currency models add result models add totals

--- src/screens/TrackerScreen.tsx
+++ src/screens/TrackerScreen.tsx
@@ -4,9 +4,18 @@ import { PageContainer } from '../components'
 
 import dataSource from '../assets/staticData.json'
 import { ColumnsType } from 'antd/lib/table';
-import { ITransactionEntry } from '../models';
+import { ITransactionEntry, ICurrency, ICalculatedResults } from '../models';
+import NumberFormat from 'react-number-format'
+import axios from 'axios';
 
 const { Title } = Typography
+
+interface IMoneyFieldProps {
+    children: number
+}
+const MoneyField = (props: IMoneyFieldProps) => {
+    return <NumberFormat value={props.children} displayType="text" thousandSeparator prefix={"$"} />
+}
 
 export const TrackerScreen = () => {
     const tableColumns: Array<ColumnsType<ITransactionEntry>> = [[
@@ -22,22 +31,32 @@ export const TrackerScreen = () => {
     ], [
         { title: "Long Term Debt", dataIndex: "account", key: "account", width: "40%" },
         { title: "", dataIndex: "monthlyPayment", key: "monthlyPayment", width: "30%" },
-        { title: "", dataIndex: "value", key: "value", width: "10%", align: "right" }
+        { title: "", dataIndex: "value", key: "value", width: "10%", align: "right", render: (val: number) => <MoneyField>{val}</MoneyField> }
     ]];
-    const currencies = [
-        "CAD",
-        "USD",
-        "EUR",
-        "HKD",
-        "KWD",
-        "LYD",
-        "JPY",
-        "INR",
-        "PKR",
-        "MYR"
+    const currencies: Array<ICurrency> = [
+        { code: "CAD", prefix: "$" },
+        { code: "USD", prefix: "$" },
+        { code: "EUR", prefix: "€" },
+        { code: "HKD", prefix: "$" },
+        { code: "KWD", prefix: "ك" },
+        { code: "LYD", prefix: "ل.د" },
+        { code: "JPY", prefix: "¥" },
+        { code: "INR", prefix: "₹" },
+        { code: "PKR", prefix: "₨" },
+        { code: "MYR", prefix: "RM" }
     ]
 
     const [transactions, setTransactions] = useState<Array<ITransactionEntry>>(dataSource as Array<ITransactionEntry>)
+    const [totalAssets, setTotalAssets] = useState<number>(transactions.filter(a => a.type === "asset").map(a => a.value).reduce((a, b) => a + b, 0))
+    const [totalLiabilities, setTotalLiabilities] = useState<number>(transactions.filter(a => a.type === "liability").map(a => a.value).reduce((a, b) => a + b, 0))
+    const [netWorth, setNetWorth] = useState<number>(totalAssets - totalLiabilities)
+
+    const calculateUpdatedValues = async () => {
+        const result = await axios.post<ICalculatedResults>(`${process.env.REACT_APP_SERVER_END_POINT}/calculate`, transactions)
+        setTotalAssets(result.data.totalAssets)
+        setTotalLiabilities(result.data.totalLiabilities)
+        setNetWorth(result.data.netWorth)
+    }
 
     return <PageContainer>
         <div style={{ display: "flex", flexDirection: "row-reverse" }}>
@@ -55,7 +74,7 @@ export const TrackerScreen = () => {
             <Col span={24}>
                 <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
                     <Title type="success" level={5}>Net Worth</Title>
-                    <Title type="success" level={5}>{`<Net Worth>`}</Title>
+                    <Title type="success" level={5}>{netWorth}</Title>
                 </div>
             </Col>
 
@@ -83,7 +102,7 @@ export const TrackerScreen = () => {
                     summary={() => (
                         <Table.Summary.Row>
                             <Table.Summary.Cell index={0}>Total Assets</Table.Summary.Cell>
-                            <Table.Summary.Cell index={1}>{`<total>`}</Table.Summary.Cell>
+                            <Table.Summary.Cell index={1}>{totalAssets}</Table.Summary.Cell>
                         </Table.Summary.Row>
                     )}
                 />
@@ -112,7 +131,7 @@ export const TrackerScreen = () => {
                     summary={() => (
                         <Table.Summary.Row>
                             <Table.Summary.Cell colSpan={2} index={0}>Total Liabilities</Table.Summary.Cell>
-                            <Table.Summary.Cell index={1}>{`<total>`}</Table.Summary.Cell>
+                            <Table.Summary.Cell index={1}>{totalLiabilities}</Table.Summary.Cell>
                         </Table.Summary.Row>
                     )}
                 />
